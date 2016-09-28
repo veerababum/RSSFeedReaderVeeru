e43d1f624bf2003a6ca0f62c56c75b2627942932
veerababu mulugu <veerababu.mulugu@gmail.com>
2016-09-27 22:47:58 -0600

    Initial Commit

---
+++ RSSFeedReaderVeeru/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSSFeedReaderVeeru
+//
+//  Created by veerababu mulugu on 9/27/16.
+//  Copyright © 2016 veerababu m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RSSFeedReaderVeeru/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RSSFeedReaderVeeru
+//
+//  Created by veerababu mulugu on 9/27/16.
+//  Copyright © 2016 veerababu m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
